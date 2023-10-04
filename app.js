b85075d84517098b90666360d68a372cd1ee7f0f
luanao <lulu-so@hotmail.com>
2023-10-04 18:35:23 -0300

    32. Routing requests

--- app.js
+++ app.js
@@ -1,10 +1,24 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-    console.log(req)
-    console.log(req.url, req.method, req.headers)
-    res.setHeader('Content-type', 'text/html')
+    //console.log(req)
+    //console.log(req.url, req.method, req.headers)
 
+    const url = req.url
+
+    if(url === '/') {
+        res.setHeader('Content-type', 'text/html')
+        res.write('<html>')
+        res.write('<head><title>Enter Message</title><head>')
+        res.write('<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></form></body>')
+        //O 'action' dentro do form tem o papel de redirecioanr para a url '/message'
+        //GET e POST são os métodos http mais importantes, porém há outros.
+        //O atributo "name" será a url para onde o usuário será redirecionado após o clique em "Send"
+        res.write('</html>')
+        return res.end()
+    }
+
+    res.setHeader('Content-type', 'text/html')
     //Permite escrever dados para a resposta
     //Pode ser escrito de uma vez ou dividido em várias linhas
     res.write('<html>')
@@ -14,7 +28,6 @@ const server = http.createServer((req, res) => {
     //A partir desse ponto, não se pode mais escrever pois
     //é o momentyo em que será enviado de volta
     res.end()
-
 
     //process.exit()
 });
