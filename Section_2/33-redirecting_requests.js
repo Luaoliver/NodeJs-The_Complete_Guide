35adda38bf881d58197200b50697a3e48efc0204
luanao <lulu-so@hotmail.com>
2023-10-10 09:25:58 -0300

    33. Redirecting requests

--- Section_2/33-redirecting_requests.js
+++ Section_2/33-redirecting_requests.js
@@ -1,5 +1,6 @@
 const http = require('http');
 const fs = require('fs');
+// fs nos permite trabalhar com sistemas de arquivos
 
 const server = http.createServer((req, res) => {
     const url = req.url
@@ -10,9 +11,6 @@ const server = http.createServer((req, res) => {
         res.write('<html>')
         res.write('<head><title>Enter Message</title><head>')
         res.write('<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></form></body>')
-        //O 'action' dentro do form tem o papel de redirecioanr para a url '/message'
-        //GET e POST são os métodos http mais importantes, porém há outros.
-        //O atributo "name" será a url para onde o usuário será redirecionado após o clique em "Send"
         res.write('</html>')
         return res.end()
     }
@@ -34,14 +32,10 @@ const server = http.createServer((req, res) => {
     }
 
     res.setHeader('Content-type', 'text/html')
-    //Permite escrever dados para a resposta
-    //Pode ser escrito de uma vez ou dividido em várias linhas
     res.write('<html>')
     res.write('<head><title>My first Page</title><head>')
     res.write('<body><h1>Hello from my Node.js Server!</h1></body>')
     res.write('</html>')
-    //A partir desse ponto, não se pode mais escrever pois
-    //é o momentyo em que será enviado de volta
     res.end()
 });
 
