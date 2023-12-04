aa1dbc29c4c865621f757823d135f1605de8d074
luanao <lulu-so@hotmail.com>
2023-12-04 19:27:08 -0300

    36. Blocking and Non-Blocking Code

--- Section_2/36-blocking_and_non_blocking_code.js
+++ Section_2/36-blocking_and_non_blocking_code.js
@@ -22,17 +22,31 @@ const server = http.createServer((req, res) => {
         })
 
         return req.on('end', () => {
+            // O req.on é nosso ouvinte de eventos, que será executado em assim que a solicitação
+            // for finalizada.
             const parseBody = Buffer.concat(body).toString()
             console.log(parseBody)
 
             const message = parseBody.split('=')[1]
 
+            // Dentro dessa função que será analisada em algum momento do futuro, temos ainda o nosso
+            // ouvinte de eventos que será executado assim que terminarmos de escrever o o arquivo.
             fs.writeFile('message.txt', message, err => {
                 res.statusCode = 302
                 res.setHeader('Location', '/')
 
                 return res.end()
             })
+
+            // Esse tipo de código é muito padrão para o nodeJs
+            // Temos uma arquitetura orientada a eventos, na qual você diz ao nodeJs que faça algo
+            // e em seguida ele transfere esse processo para o sistema operacional que utiliza
+            // multi-threading e despacha apenas pequenas ações para nunca bloquear a execução
+            // e sempre retornar quando uma função for finalizada.
+
+            // Esse é o motivo pelo qual o NodeJs possui um alto desempenho, ele nunca bloqueará seu
+            // servidor, apenas avança e diz ao sistema operacional que faça algo e retorne com a
+            // resposta que não seja uma operação de bloqueio.
         })
     }
 
