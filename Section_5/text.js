633e30b974615e8e45b1096d1e8802fbd6d657d2
luanao <lulu-so@hotmail.com>
2024-01-15 20:39:00 -0300

    57. Module Introduction

---
+++ Section_5/text.js
@@ -0,0 +1,7 @@
+// 57. Module Introduction
+
+// Neste módulo veremos o Express.js. Está é uma estrutura que pode ser
+// instalada como um pacote de terceitos no projeto node.
+// O express ajuda a terceirizar parte do trabalho de codificação, ele oferece
+// um conjunto de regras, várias funções utilitárias, entre outras coisas que
+// podem te ajudar a se concentrar no seu objetivo principal.
