a821b9dff0f48241d1af3f97e1ecd6c85d52a45f
luanao <lulu-so@hotmail.com>
2024-01-16 15:13:47 -0300

    58. What is Express.js?

--- Section_5/text.js
+++ Section_5/text.js
@@ -5,3 +5,22 @@
 // O express ajuda a terceirizar parte do trabalho de codificação, ele oferece
 // um conjunto de regras, várias funções utilitárias, entre outras coisas que
 // podem te ajudar a se concentrar no seu objetivo principal.
+
+// --------------------
+
+// 58. What is Express.js?
+
+// Escrever toda a lógica do servidor é algo muito complexo, em geral queremos
+// focar na lógica de negócio e não em pequenos detalhes de computação, para
+// isso o node utiliza um framework para todo trabalho pesado.
+// Um framework é basicamente um cojunto de funções auxiliares e ferramentas
+// com as quais podemos trabalhar.
+
+// Algumas alternativas ao ExpressJs são: Vanilla Node.js, Adonis.js, Koa,
+// Sails.js, entre vários outros.
+
+// Um ponto muito positivo do Express é que ele é altamente flexível, na verdade
+// ele não oferece muitas opções prontas para uso, mas oferece uma maneira de
+// criar seu aplicativo e trabalhar com suas funções de forma altamente
+// extensível e, por este motivo, há diversos pacotes criado por terceiros
+// para serem utilizados no Express sem a necessidade de muita configuração.
