41a9efaa98044880158ff2fd7f010ed4aaa644ea
luanao <lulu-so@hotmail.com>
2024-01-24 17:41:52 -0300

    Section 05: Assignment two - step 1 and 2

---
+++ Section_5/Assignment_two/instructions.js
@@ -0,0 +1,18 @@
+// 1. Create a npm project and install Express.js (Nodemon if you want)
+
+// 2. Create an Express.js app which funnels the requests through 2 middleware
+// functions that log something to the console and return one response.
+
+// 3. Handle request to "/" and "/users" such that each request only has one
+// handler/ middleware that does something with it (e.g. send dummy response).
+
+// --------------------
+
+// 1. Crie um projeto npm e instale o Express.js (Nodemon se desejar)
+
+// 2. Crie um aplicativo Express.js que canaliza as solicitações por meio de 2
+// funções de middleware que registram algo no console e retornam uma resposta.
+
+// 3. Lidar com a solicitação para "/" e "/users" de forma que cada solicitação
+// tenha apenas um manipulador/middleware que faça algo com ela (por exemplo,
+// enviar resposta fictícia)
---
+++ Section_5/Assignment_two/resolution.js
@@ -0,0 +1,19 @@
+// 1. Para resolver o primeiro ponto, basta instalar o Express.js, recomenda-se
+// que o faça como depêndencia de produção. Para isso, basta utilizar o
+// comando:
+//      npm install --save express
+// Mas, antes disso, lembre-se de iniciar um projeto Node.js, utilizando o
+// comando:
+//      sudo apt-get install nodejs
+// E logo em seguida, o npm, com o comando:
+//      sudo apt install npm
+
+// 2. Para iniciar um projeto express, primeiro você deve importá-lo com:
+//      const express = require('express');
+// Em seguida, deve definir o app, como:
+//      const app = express();
+// Depois, para criar funções middleware, basta utilizar 'app.use(() => {})'
+// e passar três argumentos, sendo a request, response, e o next.
+// Uma função middleware tem acesso ao objeto de solicitação (req), o objeto
+// de resposta (res) e a próxima função no ciclo de solicitação-resposta.
+// A próxima função é comumente denotada por uma variável chamada next.
