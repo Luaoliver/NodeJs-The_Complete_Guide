f598220db7e7cc6eed9f39abd9b0cc1edbf781c4
luanao <lulu-so@hotmail.com>
2024-02-20 15:32:24 -0300

    Section 05: Assigment three - step 1

---
+++ Section_5/Assignment_three/instruction.js
@@ -0,0 +1,17 @@
+// 1 - Create a npm project and install Express.js (Nodemon if you want)
+
+// 2- Create an Express.js app which serves two HTML files (of your choice/
+// with your content) for "/" and "/users"
+
+// 3 - Add some static (.js or .css) files to your project that should be
+// required by a least one of your HTML files.
+
+// --------------------
+
+// 1 - Crie um projeto npm e instale o Express.js (Nodemon se quiser)
+
+// 2- Crie um aplicativo Express.js que sirva dois arquivos HTML (de sua escolha/
+// com seu conteúdo) para "/" e "/users"
+
+// 3 - Adicione alguns arquivos estáticos (.js ou .css) ao seu projeto que devem ser
+// exigido por pelo menos um dos seus arquivos HTML.
---
+++ Section_5/Assignment_three/resolution.js
@@ -0,0 +1,9 @@
+// 1. Para resolver o primeiro ponto, basta instalar o Express.js, recomenda-se
+// que o faça como depêndencia de produção. Para isso, basta utilizar o
+// comando:
+//      npm install --save express
+// Mas, antes disso, lembre-se de iniciar um projeto Node.js, utilizando o
+// comando:
+//      sudo apt-get install nodejs
+// E logo em seguida, o npm, com o comando:
+//      sudo apt install npm
