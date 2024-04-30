5608b40e3cb4a60908cbbdea9323de74f2ca0a96
luanao <lulu-so@hotmail.com>
2024-04-30 08:29:45 -0300

    78. Module Introduction

---
+++ Section_6/text.js
@@ -0,0 +1,14 @@
+// Section 6: Working with Dynamic Content & Adding Templates Engines
+
+// 78. Module Introduction
+
+// Neste modulo trabalharemos com aquilo que o usuário vê, para que possamos
+// entregar mais valor a ele.
+// até agora o que fizemos foi trabalhar com página HTML estáticas, mas isso
+// raramente acontece em projetos reais.
+
+// Em termos práticos, veremos como gerenciar dados sem a utilização de banco
+// de dados, como renderizar conteúdos dinamicos nas Views, como utilizar
+// mecanismos de modelagem
+
+// --------------------
