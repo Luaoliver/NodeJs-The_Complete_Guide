64fc68cb2f54ab1cb2570c516f7b284f3ab46b7a
luanao <lulu-so@hotmail.com>
2024-05-09 08:21:43 -0300

    80. Templating Engines - An Overview

--- Section_6/text.js
+++ Section_6/text.js
@@ -31,3 +31,37 @@
 // afeta o objetto no geral.
 // Também é necessário modificar a maneira como está sendo exportado e 
 // importado. Pois agora nossa constante também deve ser exportada.
+
+// --------------------
+
+// 80. Templating Engines - An Overview
+
+// Para adicionar conteúdo dinâmico em nossas páginas html, nós precisamos
+// utilizar um mecanismo de modelagem. 
+
+// Temos um HTMLish, que basicamente se trata de um arquivo que possui muito
+// html porém com alguns "espaços em branco". Com a utilização do NodeJs e de
+// um mecanismo de modelagem, podemos buscar esses espaços e transformá-los
+// em um html real. O conteúdo utilizado é gerado dinamicamente no servidor
+// através do mecanismo de modelagem.
+
+// Por exemplo, com a ajuda do mecanismo de modelagem, você pode enviar uma
+// lista ordenada com itens de lista para os dados que você possui no
+// aplicativo express node e no final, você terá um resultado dinâmico, em
+// tempo real e o arquivo gerado será enviado de volta aos usuários.
+// O usuário nunca verá o modelo nem os espaços reservados, nada do que
+// acontece no servidor, ele apenas obtem uma página html, que não é
+// codificada por você e sim gerada automaticamente.
+
+// Atualmente temos várias opções de mecanismos de modelagem, mas focaremos em
+// três deles, sendo o EJS, o Pug (Jade) e Handlebars. Todos eles são conteúdos
+// gratuitos mas que atuam de diferente maneiras.
+
+// EJS <p><%= name %></p>
+// Utiliza o HTML normal e JS simples nos seus modelos.
+
+// Pug(Jade) p#{name}
+// Utiliza o mínimo de HTML e uma linguagem de modelo personalizada.
+
+// Handlebars <p>{{ name }}</p>
+// Utiliza o HTML normal e uma linguagem de modelo personalizada.
