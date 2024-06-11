041bfb4b095cb26b62d8aae3fab336ceffbf9ebf
luanao <lulu-so@hotmail.com>
2024-06-11 08:58:21 -0300

    Adjustments according to the instructor

--- routes/admin.js
+++ routes/admin.js
@@ -13,10 +13,10 @@ router.get('/add-product', (req, res, next) => {
   res.render('add-product', {
     pageTitle: 'Add Product',
     path: '/admin/add-product',
-    activeAddProduct: true,
     formsCSS: true,
-    productCSS: true
-  })
+    productCSS: true,
+    activeAddProduct: true
+  });
 });
 
 // /admin/add-product => POST
--- routes/shop.js
+++ routes/shop.js
@@ -8,15 +8,15 @@ const adminData = require('./admin');
 const router = express.Router();
 
 router.get('/', (req, res, next) => {
-  const products = adminData.products
+  const products = adminData.products;
   res.render('shop', {
     prods: products,
     pageTitle: 'Shop',
-    path:'/',
+    path: '/',
     hasProducts: products.length > 0,
     activeShop: true,
     productCSS: true
-  })
+  });
 });
 
 module.exports = router;
