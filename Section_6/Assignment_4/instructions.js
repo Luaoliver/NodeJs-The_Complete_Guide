3285caafe44b0b9a58df4de400141ac9d8ec9769
luanao <lulu-so@hotmail.com>
2024-06-12 08:18:05 -0300

    Section 06: Exercises

---
+++ Section_6/Assignment_4/instructions.js
@@ -0,0 +1,5 @@
+// 1. Create a npm project and install Express.js (Nodemon if you want).
+
+// 2. Add two routes:
+//      '/' => Holds a <form> that allows users to input their name.
+//      'users' => Outputs an <ul> with the users names (or some error text).
---
+++ Section_6/Assignment_4/resolution.js
@@ -0,0 +1,13 @@
+// 1. Para resolver o primero requisito, vamos criar um novo projeto e instalar
+// o Express.js.
+// Para iniciar um projeto node, utilizamos:
+//      sudo apt-get install nodejs
+// E em seguida, execute:
+//      sudo apt install npm
+// Para o express.js, O recomendado é que o faça como dependencia de produção.
+// Para isso, basta utilizar o comando:
+//      npm install --save express ejs body-parser
+// Também lembre-se de adicionar o nodemon com:
+//      npm install --save-dev nodemon
+
+// 2. Para o segundo requisito,
