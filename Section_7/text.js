ad114a88653a9b6411fbf5cdce94c9fa44082f7c
luanao <lulu-so@hotmail.com>
2024-06-17 08:14:44 -0300

    97. Module Introduction

---
+++ Section_7/text.js
@@ -0,0 +1,6 @@
+// Section 07: The Model View Controller (MVC)
+
+// 97. Module Introduction
+
+// Neste módulo vamos aprender maneiras de estruturar nosso código. Como
+// também alguns conceitos por trás disso.
