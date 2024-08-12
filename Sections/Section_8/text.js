a8f06332e021139cb17c3b6fe35cae2bf21d83f1
luanao <lulu-so@hotmail.com>
2024-08-12 09:29:07 -0300

    Section 08: Optional: Enhancing the app

---
+++ Sections/Section_8/text.js
@@ -0,0 +1,9 @@
+// Section 08: Optional: Enhancing the app
+
+// 107. Module Introduction
+
+// Este módulo existe para que seja praticado tudo aquilo que foi ensinado nos
+// módulos anteriores.
+// Também vamos avançar um pouco para que a nossa loja virtual pareça mais realista.
+
+// Este módulo é completamente opcional, caso não esteja interessado em praticar.
