2d2933f359cd40ffda9783738b41b3c8a9eb3a37
luanao <lulu-so@hotmail.com>
2024-08-20 08:57:19 -0300

    108. Creating the Shop Structure

--- Sections/Section_8/text.js
+++ Sections/Section_8/text.js
@@ -7,3 +7,12 @@
 // Também vamos avançar um pouco para que a nossa loja virtual pareça mais realista.
 
 // Este módulo é completamente opcional, caso não esteja interessado em praticar.
+
+// --------------------
+
+// 108. Creating the Shop Structure
+
+// No fim, o nosso projeto deve ser uma loja online e para isso, precisamos uma página
+// inicial no qual possamos ver todos os produtos listados, também devemos poder
+// adicionar novos produtos e excluí-los, adicionar produtos a um carrinho e finalizar
+// o checkout, pagando pelos produtos.
