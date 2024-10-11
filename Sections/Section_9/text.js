f61c475e5e5db64ad5bf23ad54d728a60884fd18
luanao <lulu-so@hotmail.com>
2024-10-11 17:20:58 -0300

    116. Module Introduction

--- Sections/Section_9/text.js
+++ Sections/Section_9/text.js
@@ -1,1 +1,8 @@
 // Section 09: Dynamic Routes & Advanced Models
+
+// 116. Module Introduction
+
+// Passing and Using Dynamic Data.
+// Passing Route Params.
+// Using Query Params.
+// Enhance our Models.
