f38e9b57909b9a7460a40c19f37b996004ce705c
luanao <lulu-so@hotmail.com>
2025-03-25 19:38:46 -0300

    136. Module Introduction

---
+++ Sections/Section_10/text.js
@@ -0,0 +1,12 @@
+// Section 10: SQL Introduction
+
+// 136. Module Introduction
+
+// Storing Data in Databases.
+// Storing Application Data Correctly.
+
+// Armazenar algo em arquivos é algo que definitivamente não fariamos em um aplicativo
+// real (ou ao meenos não deveriamos).
+
+// In this module we will see differente kinds of database (SQL vs NoSQL).
+// How to use a SQL in a Node.js App.
